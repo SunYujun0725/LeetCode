95910fc7eb098d5f02f5138bfb1a17d7db1bb320
Yu-Chun Sun <91317039+SunYujun0725@users.noreply.github.com>
2024-02-04 13:39:24 +0800

    Add files via upload

---
+++ Power_of_Four.py
@@ -0,0 +1,17 @@
+'''
+Author: SunYujun0725 91317039+SunYujun0725@users.noreply.github.com
+Date: 2024-02-01 19:48:33
+LastEditors: SunYujun0725 91317039+SunYujun0725@users.noreply.github.com
+LastEditTime: 2024-02-04 13:38:52
+FilePath: /vscode_cpp_for_Mac-master/Power_of_Three.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        i = 0
+        while (4**i <= n):
+            if (4**i == n):
+                return True
+            i += 1
+        return False
+            
