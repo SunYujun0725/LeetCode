193364cb3a6df75bdba5a1f5a10088e577854373
Yu-Chun Sun <91317039+SunYujun0725@users.noreply.github.com>
2024-02-05 22:36:15 +0800

    Add files via upload

--- Construct_the_Rectangle.py
+++ Construct_the_Rectangle.py
@@ -2,7 +2,7 @@
 Author: SunYujun0725 91317039+SunYujun0725@users.noreply.github.com
 Date: 2024-02-01 19:48:33
 LastEditors: SunYujun0725 91317039+SunYujun0725@users.noreply.github.com
-LastEditTime: 2024-02-05 22:32:10
+LastEditTime: 2024-02-05 22:36:00
 FilePath: /vscode_cpp_for_Mac-master/Construct_the_Rectangle.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -21,6 +21,8 @@ class Solution:
                         L = W
                         W = temp
                     min = L - W
+            else:
+                break
         ans.append(L)
         ans.append(W)
         return ans
